6a7ad65aa47d11790b087c85d3a51e76eca1143b
Xyfuture <49277529+Xyfuture@users.noreply.github.com>
2022-11-09 11:21:04 +0800

    change process

--- sim/core/compo/inst_fetch.py
+++ sim/core/compo/inst_fetch.py
@@ -40,28 +40,29 @@ class InstFetch(BaseCoreCompo):
 
 
     def process(self):
-        self.fetch_inst()
-        self.update_pc()
+        pc = self._pc.read()
+        jump_payload = self.jump_pc.read(self.current_time)
 
 
-    def fetch_inst(self):
-        jump_payload = self.jump_pc.read()
+        current_inst = self.fetch_inst(pc,jump_payload)
+        next_pc = self.update_pc()
 
+        self.if_id_port.write(current_inst,self.next_update_epslion)
+        self._pc_in.write(next_pc,self.next_update_epsilon)
+
+
+    def fetch_inst(self,pc,jump_payload):
+        inst_payload = {'pc':pc,'inst':self._inst_buffer[pc]}        
         if jump_payload:
-            self.if_id_port.write({'pc':-1,'inst':None},self.next_update_epslion)
-        else:
-            pc = self._pc.read()
-            inst = self._inst_buffer[pc]
-            self.if_id_port.write({'pc':pc,'inst':inst},self.next_update_epslion)
+            inst_payload = {'pc':-1,'inst':None}
+        return inst_payload
 
-    def update_pc(self):
-        jump_payload = self.jump_pc.read()
+    def update_pc(self,pc,jump_payload):
+        next_pc = pc + 1 
         # 如果这个周期内没发送jump,那么就不更新
         if jump_payload :
-            self._pc_in.write(jump_payload['pc'],self.next_update_epslion)
-        else:
-            old_pc = self._pc.read()
-            self._pc_in.write(old_pc + 1,self.next_update_epslion)
+            next_pc = jump_payload['pc']
+        return next_pc
 
 
     def initialize(self):
