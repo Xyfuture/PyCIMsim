d758ba393dfc7df3da8237b33c23de9f1d9b5171
xyfuture <xyfuture@foxmail.com>
2024-02-27 22:01:19 +0800

    finish transfer design

--- sim/core/compo/transfer.py
+++ sim/core/compo/transfer.py
@@ -6,7 +6,7 @@ from sim.circuit.wire.wire import InWire, UniWire, OutWire
 from sim.circuit.register.register import RegNext
 from sim.config.config import CoreConfig
 from sim.core.compo.base_core_compo import BaseCoreCompo
-from sim.core.compo.connection.payloads import TransferInfo, BusPayload, MemoryRequest, SyncMessage, InterCoreMessage
+from sim.core.compo.connection.payloads import TransferInfo, BusPayload, MemoryRequest, InterCoreMessage
 
 from sim.core.compo.message_bus import MessageInterface
 from sim.core.utils.payload.base import PayloadBase
@@ -117,16 +117,39 @@ class TransferUnit(BaseCoreCompo):
         else:
             assert False
 
+    @registry(['func_trigger'])
+    def process(self):
+        transfer_info: TransferInfo = self._fsm_reg_output.read().transfer_info
+
+        op = transfer_info.op
+        self._run_state = op
+
+        if __debug__:
+            self.pfc.begin(op)
+
+        if op == 'send':
+            self.cur_send_coroutine = self.process_send(transfer_info)
+            next(self.cur_recv_coroutine)
+        elif op == 'recv':
+            self.cur_recv_coroutine = self.process_receive(transfer_info)
+            next(self.cur_recv_coroutine)
+        else:
+            # 内存操作有关指令
+            self.cur_memory_coroutine = self.process_memory(transfer_info)
+            next(self.cur_memory_coroutine)
+
+        self.circuit_add_dynamic_energy(self._config.transfer_energy)
+
     # local_buffer receive
     @registry(['transfer_buffer'])
     def local_buffer_receive_dispatch(self, bus_payload: BusPayload):
-        payload = bus_payload.payload # 可以对这个进行一下check
+        payload = bus_payload.payload  # 可以对这个进行一下check
         transfer_info: TransferInfo = self._fsm_reg_output.read().transfer_info
 
         if transfer_info.op == 'send':
             next(self.cur_send_coroutine)
         elif transfer_info.op == 'recv':
-            next(self.cur_recv_coroutine) # 貌似不会出现
+            next(self.cur_recv_coroutine)  # 貌似不会出现
         else:
             next(self.cur_memory_coroutine)
 
@@ -173,137 +196,7 @@ class TransferUnit(BaseCoreCompo):
             # memory 相关操作的指令
             next(self.cur_memory_coroutine)
 
-        self.noc_interface.allow_receive() # 传输结束，开始下一次 （可修改为等一段时间在开始下一次
-
-    # 发射指令
-    @registry(['func_trigger'])
-    def process(self):
-        transfer_info: TransferInfo = self._fsm_reg_output.read().transfer_info
-
-        op = transfer_info.op
-        self._run_state = op
-
-        if __debug__:
-            self.pfc.begin(op)
-
-        if op == 'sync':
-            self.execute_sync(transfer_info)
-        elif op == 'wait_core':
-            self.execute_wait_core(transfer_info)
-        elif op == 'inc_state':
-            self.execute_inc_state(transfer_info)
-        else:
-            self.execute_memory(transfer_info)
-
-        self.circuit_add_dynamic_energy(self._config.transfer_energy)
-
-    def execute_sync(self, transfer_info: TransferInfo):
-        start_core, end_core = transfer_info.sync_info['start_core'], transfer_info.sync_info['end_core']
-        self.is_sync_master_core = (start_core == self.core_id)
-
-        if self.is_sync_master_core:
-            if start_core == end_core:
-                self.is_sync_master_core = False
-                self.finish_execute()
-            else:
-                for core in range(start_core + 1, end_core + 1):
-                    self.sync_dict[core] = False
-                for core in self.sync_dict:
-                    query_sync_request = BusPayload(
-                        src=self.core_id, dst=core, data_size=1,
-                        payload=SyncMessage(
-                            op='sync', message='query'
-                        )
-                    )
-                    self.noc_interface.send(query_sync_request, None)
-        else:
-            self.slave_core_start_sync()
-
-
-    def execute_memory(self, transfer_info: TransferInfo):
-
-        op = transfer_info.op
-        if op == 'dram_to_global':
-            dram_read_reqeust = BusPayload(
-                src=self.core_id, dst='dram', data_size=1,
-                payload=MemoryRequest(
-                    access_type='read', addr=transfer_info.mem_access_info.src_addr,
-                    data_size=transfer_info.mem_access_info.data_size
-                )
-            )
-            self.noc_interface.send(dram_read_reqeust, None)
-        elif op in ['global_to_local', 'global_to_dram', 'global_cpy']:
-            global_read_reqeust = BusPayload(
-                src=self.core_id, dst='global', data_size=1,
-                payload=MemoryRequest(
-                    access_type='read', addr=transfer_info.mem_access_info.src_addr,
-                    data_size=transfer_info.mem_access_info.data_size
-                )
-            )
-            self.noc_interface.send(global_read_reqeust, None)
-        elif op in ['local_to_global', 'local_cpy']:
-            local_read_request = BusPayload(
-                src='transfer', dst='buffer', data_size=1,
-                payload=MemoryRequest(
-                    access_type='read', addr=transfer_info.mem_access_info.src_addr,
-                    data_size=transfer_info.mem_access_info.data_size
-                )
-            )
-            self.transfer_buffer.send(local_read_request, None)
-        elif op == 'global_clr':
-            global_write_request = BusPayload(
-                src=self.core_id, dst='global', data_size=transfer_info.mem_access_info.data_size,
-                payload=MemoryRequest(
-                    access_type='write', addr=transfer_info.mem_access_info.src_addr,
-                    data_size=transfer_info.mem_access_info.data_size
-                )
-            )
-            self.noc_interface.send(global_write_request, self.finish_execute)
-        elif op == 'local_clr':
-            local_write_request = BusPayload(
-                src='transfer', dst='buffer', data_size=transfer_info.mem_access_info.data_size,
-                payload=MemoryRequest(
-                    access_type='write', addr=transfer_info.mem_access_info.src_addr,
-                    data_size=transfer_info.mem_access_info.data_size
-                )
-            )
-            self.transfer_buffer.send(local_write_request, self.finish_execute)
-
-    def memory_receive_handler(self, bus_payload: BusPayload):
-        # 第二次执行时的操作,读取的数据到了,转发到第二个
-        fsm_payload = self._fsm_reg_output.read()
-        transfer_info: TransferInfo = fsm_payload.transfer_info
-
-        op = transfer_info.op
-        if op in ['global_to_local', 'local_cpy']:
-            local_write_request = BusPayload(
-                src='transfer', dst='buffer', data_size=transfer_info.mem_access_info.data_size,
-                payload=MemoryRequest(
-                    access_type='write', addr=transfer_info.mem_access_info.dst_addr,
-                    data_size=transfer_info.mem_access_info.data_size
-                )
-            )
-            self.transfer_buffer.send(local_write_request, self.finish_execute)
-        elif op in ['dram_to_global', 'local_to_global', 'global_cpy']:
-            global_write_request = BusPayload(
-                src=self.core_id, dst='global', data_size=transfer_info.mem_access_info.data_size,
-                payload=MemoryRequest(
-                    access_type='write', addr=transfer_info.mem_access_info.dst_addr,
-                    data_size=transfer_info.mem_access_info.data_size
-                )
-            )
-            self.noc_interface.send(global_write_request, self.finish_execute)
-        elif op == 'global_to_dram':
-            dram_write_request = BusPayload(
-                src=self.core_id, dst='dram', data_size=transfer_info.mem_access_info.data_size,
-                payload=MemoryRequest(
-                    access_type='write', addr=transfer_info.mem_access_info.dst_addr,
-                    data_size=transfer_info.mem_access_info.data_size
-                )
-            )
-            self.noc_interface.send(dram_write_request, self.finish_execute)
-        else:
-            assert False
+        self.noc_interface.allow_receive()  # 传输结束，开始下一次 （可修改为等一段时间在开始下一次
 
     def finish_execute(self):
 
@@ -355,6 +248,9 @@ class TransferUnit(BaseCoreCompo):
 
         self.noc_interface.send(send_data_request, self.finish_execute)
 
+        yield ('send data to dst core'
+               'execution finish')
+
     def process_receive(self, transfer_info: TransferInfo):
         # TODO 有bug 关于 allow receive的
 
@@ -390,15 +286,18 @@ class TransferUnit(BaseCoreCompo):
 
         self.transfer_buffer.send(local_memory_write_request, self.finish_execute())
 
-    def process_memory(self,transfer_info: TransferInfo):
+        yield ('receive data from src core'
+               'execution finish')
+
+    def process_memory(self, transfer_info: TransferInfo):
         op = transfer_info.op
 
         dram_read_request = BusPayload(
-                src=self.core_id, dst='dram', data_size=1,
-                payload=MemoryRequest(
-                    access_type='read', addr=transfer_info.src1_addr,
-                    data_size=transfer_info.len
-                )
+            src=self.core_id, dst='dram', data_size=1,
+            payload=MemoryRequest(
+                access_type='read', addr=transfer_info.src1_addr,
+                data_size=transfer_info.len
+            )
         )
 
         dram_write_request = BusPayload(
@@ -410,41 +309,43 @@ class TransferUnit(BaseCoreCompo):
         )
 
         local_read_request = BusPayload(
-                src='transfer', dst='buffer', data_size=1,
-                payload=MemoryRequest(
-                    access_type='read', addr=transfer_info.src1_addr,
-                    data_size=transfer_info.len
-                )
+            src='transfer', dst='buffer', data_size=1,
+            payload=MemoryRequest(
+                access_type='read', addr=transfer_info.src1_addr,
+                data_size=transfer_info.len
+            )
         )
 
         local_write_request = BusPayload(
-                src='transfer', dst='buffer', data_size=transfer_info.len,
-                payload=MemoryRequest(
-                    access_type='write', addr=transfer_info.dst_addr,
-                    data_size=transfer_info.len
-                )
+            src='transfer', dst='buffer', data_size=transfer_info.len,
+            payload=MemoryRequest(
+                access_type='write', addr=transfer_info.dst_addr,
+                data_size=transfer_info.len
             )
+        )
 
         if op == 'dram_to_local':
-            self.noc_interface.send(dram_read_request,None)
+            self.noc_interface.send(dram_read_request, None)
             yield 'dram read req send'
-            self.transfer_buffer.send(local_write_request,self.finish_execute)
+            self.transfer_buffer.send(local_write_request, self.finish_execute)
         elif op == 'local_to_dram':
-            self.transfer_buffer.send(local_write_request,None)
+            self.transfer_buffer.send(local_write_request, None)
             yield 'local buffer read req send'
-            self.noc_interface.send(dram_write_request,self.finish_execute)
+            self.noc_interface.send(dram_write_request, self.finish_execute)
         elif op == 'dram_clr':
-            self.noc_interface.send(dram_write_request,self.finish_execute)
+            self.noc_interface.send(dram_write_request, self.finish_execute)
         elif op == 'local_clr':
-            self.transfer_buffer.send(local_write_request,self.finish_execute)
+            self.transfer_buffer.send(local_write_request, self.finish_execute)
         elif op == 'dram_cpy':
-            self.noc_interface.send(dram_read_request,None)
+            self.noc_interface.send(dram_read_request, None)
             yield 'dram cpy read req send'
-            self.noc_interface.send(dram_write_request,self.finish_execute)
+            self.noc_interface.send(dram_write_request, self.finish_execute)
         elif op == 'local_cpy':
-            self.transfer_buffer.send(local_read_request,None)
+            self.transfer_buffer.send(local_read_request, None)
             yield 'local cpy read req send'
-            self.transfer_buffer.send(local_write_request,self.finish_execute)
+            self.transfer_buffer.send(local_write_request, self.finish_execute)
+
+        yield 'execution finish'
 
     def get_running_status(self):
         core_id = 0
@@ -454,8 +355,3 @@ class TransferUnit(BaseCoreCompo):
         fsm_info = self._fsm_reg_output.read()
         info = f"Core:{core_id} TransferUnit> {fsm_info}"
         return info
-
-# 一些格式信息
-# {'src':1,'dst':2,'data_size':1,'op':'sync','message':'start/finish','sync_cnt':sync_cnt}
-# {'src':1,'dst':2,'data_size':1,'op':'wait_core','message':'finish','state':self.state_value}
-# {'src':1,'dst':2,'data_size':1,'op':'wait_core','message':'start','state':self.wait_state}
